8ca80e82ccd9bb06fa78d097ffb9e152aec2f7e5
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-08 02:25:05 +0600

    student  class

---
+++ classTwo.ts
@@ -0,0 +1,14 @@
+class Student {
+    name: string;
+    age: number;
+    address: string
+
+    constructor (name: string, age: number, address: string){
+        this.name = name,
+        this.address = address
+        this.age = age
+    }
+    getSleep() {
+        console.log(`ghum`)
+    }
+}
