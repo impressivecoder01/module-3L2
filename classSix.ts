8756c59cd6d29a16338a83a73efaab2e01ea27a3
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-09 00:41:22 +0600

    get and set balance by function

--- classSix.ts
+++ classSix.ts
@@ -1,1 +1,21 @@
 // getter and setter
+class BankAccount2 {
+    public userId: number;
+    public userName :string;
+    // private userBalance: number;
+    protected userBalance: number;
+    constructor(userId: number, userName: string, userBalance: number){
+        this.userId= userId,
+        this.userName= userName
+        this.userBalance= userBalance
+    }
+    addBalance(balance: number){
+     this.userBalance = this.userBalance + balance
+    }
+    getBalance(){
+        return this.userBalance
+    }
+}
+const rohanBankAccount = new BankAccount2(22,'33', 3)
+rohanBankAccount.addBalance(30)
+console.log(rohanBankAccount.getBalance());
