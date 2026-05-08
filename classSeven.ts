5d03ebd9b368ff6e45b8625149f94e35acb41b11
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-09 01:04:35 +0600

    static memory

---
+++ classSeven.ts
@@ -0,0 +1,29 @@
+// static in ts
+// class Counter {
+//     static count : number = 0;
+//     increment(){
+//         return (Counter.count = Counter.count + 1)
+//     }
+//     decrement(){
+//         return (Counter.count = Counter.count - 1)
+//     }
+// }
+// const instance1 = new Counter()
+// console.log(instance1.increment());
+// console.log(instance1.increment());
+// console.log(instance1.increment());
+
+// const instance2 = new Counter ()
+// console.log(instance2.increment());
+class Counter {
+    static count : number = 0;
+    static increment(){
+        return (Counter.count = Counter.count + 1)
+    }
+    static decrement(){
+        return (Counter.count = Counter.count - 1)
+    }
+}
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.decrement());
