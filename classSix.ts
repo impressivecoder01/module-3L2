3f8cfd32b910af98b00a87a790a9ad6b2aa62bf0
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-09 00:50:42 +0600

    getter and setter

--- classSix.ts
+++ classSix.ts
@@ -9,13 +9,25 @@ class BankAccount2 {
         this.userName= userName
         this.userBalance= userBalance
     }
-    addBalance(balance: number){
-     this.userBalance = this.userBalance + balance
+    // addBalance(balance: number){
+    // return this.userBalance = this.userBalance + balance
+    // }
+    // doing this using setter
+    set addBalance(amount: number){
+        this.userBalance = this.userBalance + amount
     }
-    getBalance(){
+    // getBalance(){
+    //     return this.userBalance
+    // }
+    // get this by getter
+    get getBalance(){
         return this.userBalance
     }
 }
 const rohanBankAccount = new BankAccount2(22,'33', 3)
-rohanBankAccount.addBalance(30)
-console.log(rohanBankAccount.getBalance());
+// rohanBankAccount.addBalance(30)
+// console.log(rohanBankAccount.getBalance());
+rohanBankAccount.addBalance = 1000
+rohanBankAccount.addBalance = 1000
+// console.log(rohanBankAccount);
+console.log(rohanBankAccount.getBalance);
