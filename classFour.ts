47048ffd620e019942d788aef87abf773032174b
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-08 21:03:48 +0600

    classForu

--- classFour.ts
+++ classFour.ts
@@ -26,5 +26,26 @@ class Teacher4 extends Person{
     }
 }
 
+//function guard
+const isStudent = (user: Person) => {
+    return user instanceof Student4
+}
+const isTeacher = (user: Person) => {
+    return user instanceof Teacher4
+}
 
-const getUserInfo = 
+const getUserInfo = (user: Person) => {
+    if(isStudent(user)){
+        user.doStudy(10)
+    }else if(isTeacher(user)){
+        user.takeClass(23)
+    }
+    else{
+        user.getSleep(2)
+    }
+}
+
+const studentOne = new Student4('mr.student4')
+const teacherOne = new Teacher4('mr.teacher')
+getUserInfo(studentOne)
+getUserInfo(teacherOne)
