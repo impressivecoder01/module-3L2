8e897c9ed869ea010314c2c1343ced6fbcaca00a
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-09 23:23:50 +0600

    class eleven

--- classEleven.ts
+++ classEleven.ts
@@ -21,3 +21,25 @@ class MusicPlayer implements MediaPlayer{
 }
 const myMusicPlayer = new MusicPlayer()
 myMusicPlayer.play()
+
+// idea
+abstract class MediaPlayer2{
+    abstract play() :void;
+    abstract pause(): void;
+    abstract stop(): void;
+}
+class MusicPlayer2 extends MediaPlayer2{
+    play(): void {
+        console.log('play');
+    }
+    pause(): void {
+        console.log('pause');
+    }
+    stop(): void {
+        console.log('stop');
+    }
+}
+const myMusicPlayer2 = new MusicPlayer2()
+myMusicPlayer2.pause()
+myMusicPlayer2.play()
+myMusicPlayer2.stop()
